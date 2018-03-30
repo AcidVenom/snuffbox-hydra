900ecf39f06b64790fa3d2ba6f429834452f10e5
Daniel Konings <konings_304@hotmail.com>
2018-03-31 00:24:18 +0200

    - Forgot to clean up main.cc again

--- src/test/main.cc
+++ src/test/main.cc
@@ -4,38 +4,7 @@
 using namespace snuffbox;
 using namespace foundation;
 
-class Test
-{
-
-public:
-
-  Test(int v) :
-    test_(v)
-  {
-
-  }
-
-  int test() const
-  {
-    return test_;
-  }
-
-  ~Test()
-  {
-    Logger::Log(Logger::Channel::kPlayer, Logger::Verbosity::kDebug, "Destructed..");
-  }
-
-private:
-
-  int test_;
-};
-
 int main(int argc, char** argv)
 {
-  Test* test = Memory::Construct<Test>(1000);
-  Logger::Log(Logger::Channel::kPlayer, Logger::Verbosity::kDebug, "{0}", test->test());
-  Memory::Destruct(test);
-
-  std::cin.get();
   return 0;
 }
