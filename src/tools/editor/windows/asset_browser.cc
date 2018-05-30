c682bc92ed833bbafeb58d4a77872b500c775150
Daniel Konings <konings_304@hotmail.com>
2018-05-30 18:06:53 +0200

    - More work on the asset browser, added AssetBrowserItem - Moving onto actually showing assets from within the directory tree now

--- src/tools/editor/windows/asset_browser.cc
+++ src/tools/editor/windows/asset_browser.cc
@@ -1,19 +1,85 @@
 #include "tools/editor/windows/asset_browser.h"
+#include "tools/editor/definitions/editor_colors.h"
 
 #include <qfilesystemmodel.h>
 #include <qtreeview.h>
+#include <qgridlayout.h>
+#include <qlabel.h>
 
 namespace snuffbox
 {
   namespace editor
   {
     //--------------------------------------------------------------------------
-    AssetBrowser::AssetBrowser(QTreeView* tree, QLayout* assets) :
+    const int AssetBrowserItem::kIconSize_ = 64;
+    const int AssetBrowserItem::kLabelHeight_ = 20;
+
+    const int AssetBrowser::kItemsPerRow_ = 3;
+    const int AssetBrowser::kContentMargin_ = 10;
+
+    //--------------------------------------------------------------------------
+    AssetBrowserItem::AssetBrowserItem(const QString& name) :
+      icon_(nullptr),
+      label_(nullptr)
+    {
+      CreateItem(name);
+      ApplyStyle();
+    }
+
+    //--------------------------------------------------------------------------
+    void AssetBrowserItem::CreateItem(const QString& name)
+    {
+      QWidget* icon_wrapper = new QWidget();
+      QVBoxLayout* wrapper_layout = new QVBoxLayout();
+
+      icon_wrapper->setLayout(wrapper_layout);
+
+      icon_ = new QFrame();
+      icon_->setSizePolicy({ QSizePolicy::Fixed, QSizePolicy::Fixed });
+      icon_->setFixedSize(QSize{ kIconSize_, kIconSize_ });
+
+      wrapper_layout->setAlignment(Qt::AlignHCenter);
+      wrapper_layout->addWidget(icon_);
+      wrapper_layout->addStretch();
+
+      label_ = new QLabel();
+      label_->setText(name);
+      label_->setFixedSize(QSize{ kIconSize_ * 2, kLabelHeight_ });
+      label_->setSizePolicy({ QSizePolicy::Fixed, QSizePolicy::Fixed });
+      label_->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+
+      QVBoxLayout* widget_layout = new QVBoxLayout(this);
+      widget_layout->setAlignment(Qt::AlignHCenter);
+
+      widget_layout->addWidget(icon_wrapper);
+      widget_layout->addWidget(label_);
+      widget_layout->addStretch();
+
+      setLayout(widget_layout);
+    }
+
+    //--------------------------------------------------------------------------
+    void AssetBrowserItem::ApplyStyle()
+    {
+      QString bg_col = EditorColors::ColorToCSS(
+        EditorColors::DefaultPalette().color(QPalette::Base));
+
+      icon_->setStyleSheet(
+        "QFrame{ background-color: " + bg_col + "; border-radius: 4px; }");
+
+      bg_col = EditorColors::ColorToCSS(EditorColors::DockColor());
+      label_->setStyleSheet(
+        "QLabel{ background-color: " + bg_col + "; border-radius: 4px; }");
+    }
+
+    //--------------------------------------------------------------------------
+    AssetBrowser::AssetBrowser(QTreeView* tree, QGridLayout* assets) :
       tree_(tree),
       assets_(assets),
       model_(nullptr)
     {
-      
+      ApplyStyle();
+      TestAssets();
     }
 
     //--------------------------------------------------------------------------
@@ -35,6 +101,30 @@ namespace snuffbox
 
       tree_->setModel(model_);
       tree_->setRootIndex(model_->index(root));
+    }
+
+    //--------------------------------------------------------------------------
+    void AssetBrowser::TestAssets()
+    {
+      //!< @todo Remove this function when I've implemented showing assets
+
+      const int num_rows = 2;
+
+      for (int r = 0; r < num_rows; ++r)
+      {
+        for (int c = 0; c < kItemsPerRow_; ++c)
+        {
+          assets_->addWidget(new AssetBrowserItem("test_item"), r, c);
+        }
+      }
+
+      assets_->setRowStretch(num_rows - 1, 1);
+    }
+
+    //--------------------------------------------------------------------------
+    void AssetBrowser::ApplyStyle()
+    {
+      assets_->setMargin(kContentMargin_);
     }
   }
 }
--- src/tools/editor/windows/asset_browser.h
+++ src/tools/editor/windows/asset_browser.h
@@ -3,15 +3,64 @@
 #include <foundation/io/path.h>
 
 #include <qobject.h>
+#include <qwidget.h>
 
 class QTreeView;
-class QLayout;
+class QGridLayout;
 class QFileSystemModel;
+class QFrame;
+class QLabel;
 
 namespace snuffbox
 {
   namespace editor
   {
+    /**
+    * @brief An asset browser item to show in the asset browser
+    *
+    * The asset browser items have a label and an icon, of which the icon
+    * is in correlation to the asset type.
+    *
+    * The item also contains a reference to the path the item belongs to,
+    * so it can be dragged onto areas in the interface where paths are required.
+    *
+    * @author Daniel Konings
+    */
+    class AssetBrowserItem : public QWidget
+    {
+
+    public:
+
+      /**
+      * @brief Construct through a name
+      *
+      * @param[in] name The name of the asset
+      */
+      AssetBrowserItem(const QString& name);
+
+    protected:
+
+      /**
+      * @brief Creates the actual underlying widgets
+      *
+      * @param[in] name The name to put at the label field
+      */
+      void CreateItem(const QString& name);
+
+      /**
+      * @brief Applies the colors to the created label and icon
+      */
+      void ApplyStyle();
+
+    private:
+
+      QFrame* icon_; //!< The created icon
+      QLabel* label_; //!< The created label
+
+      static const int kIconSize_; //!< The squared icon size
+      static const int kLabelHeight_; //!< The label heights
+    };
+
     /**
     * @brief The asset browser that shows all assets currently in the build
     *        directory
@@ -32,7 +81,7 @@ namespace snuffbox
       * @param[in] tree The tree view to show the build directories in
       * @param[in] assets The assets browser layout
       */
-      AssetBrowser(QTreeView* tree, QLayout* assets);
+      AssetBrowser(QTreeView* tree, QGridLayout* assets);
 
       /**
       * @brief Refresh the asset browser by path
@@ -41,12 +90,27 @@ namespace snuffbox
       */
       void Refresh(const foundation::Path& path);
 
+    protected:
+
+      /**
+      * @brief Debug functionality to test the asset browser interface
+      */
+      void TestAssets();
+
+      /**
+      * @brief Applies the styles for the asset browser
+      */
+      void ApplyStyle();
+
     private:
 
       QTreeView* tree_; //!< The tree view to show the build directories in
-      QLayout* assets_; //!< The assets browser layout
+      QGridLayout* assets_; //!< The assets browser layout
 
       QFileSystemModel* model_; //!< The file system model to show directories
+
+      static const int kItemsPerRow_; //!< The number of items per row
+      static const int kContentMargin_; //!< The margin between assets
     };
   }
 }
