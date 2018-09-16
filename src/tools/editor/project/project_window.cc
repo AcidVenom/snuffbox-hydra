7c37fb031b99ee072381158528597397b0e9d21e
Daniel Konings <konings_304@hotmail.com>
2018-09-16 16:13:25 +0200

    - Saving of recent projects, creation of new projects with a name

--- src/tools/editor/project/project_window.cc
+++ src/tools/editor/project/project_window.cc
@@ -7,6 +7,12 @@
 #include <QLabel>
 #include <QSettings>
 #include <QFileDialog>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QFile>
+#include <QFlags>
+#include <QMessageBox>
+#include <QLineEdit>
 
 namespace snuffbox
 {
@@ -15,13 +21,26 @@ namespace snuffbox
     //--------------------------------------------------------------------------
     const int ProjectWindow::kProjectWindowWidth_ = 640;
     const int ProjectWindow::kProjectWindowHeight_ = 320;
-    const QString ProjectWindow::kSettingsRecentFolder_ = "Project.Recent";
+
+    const QString ProjectWindow::kSettingsRecentFolder_ = 
+      "Project.RecentFolder";
+    const QString ProjectWindow::kSettingsRecentlyOpened_ = 
+      "Project.RecentlyOpened.%0";
+
+    const QString ProjectWindow::kProjectFile_ = "project_settings.json";
+
+    const int ProjectWindow::kMaxRecentlyOpened_ = 5;
+
+    //--------------------------------------------------------------------------
+    const int NewProjectDialog::kNewProjectDialogWidth_ = 480;
+    const int NewProjectDialog::kNewProjectDialogHeight_ = 96;
 
     //--------------------------------------------------------------------------
     ProjectWindow::ProjectWindow(Project* project, QWidget* parent) :
       QDialog(parent),
       project_(project),
-      recent_folder_("")
+      recent_folder_(""),
+      recent_projects_(nullptr)
     {
       setObjectName(QStringLiteral("ProjectWindow"));
 
@@ -40,12 +59,12 @@ namespace snuffbox
       main_layout->addLayout(recent_layout);
 
       QLabel* recent_label = 
-        new QLabel(QStringLiteral("Recent projects"), recent_layout->widget());
+        new QLabel(QStringLiteral("Recently opened"), recent_layout->widget());
 
       recent_layout->addWidget(recent_label);
 
-      QListWidget* recent_projects = new QListWidget(recent_layout->widget());
-      recent_projects->setObjectName(QStringLiteral("RecentProjectsWidget"));
+      recent_projects_ = new QListWidget(recent_layout->widget());
+      recent_projects_->setObjectName(QStringLiteral("RecentProjectsWidget"));
 
       QHBoxLayout* buttons_layout = new QHBoxLayout(main_layout->widget());
       buttons_layout->setObjectName(QStringLiteral("ProjectButtonsLayout"));
@@ -68,31 +87,174 @@ namespace snuffbox
 
       main_layout->addLayout(buttons_layout);
 
-      recent_layout->addWidget(recent_projects);
+      recent_layout->addWidget(recent_projects_);
 
       connect(
-        button_new, &QPushButton::clicked, this, &ProjectWindow::NewProject);
+        button_new, 
+        &QPushButton::clicked,
+        this,
+        &ProjectWindow::OnNewProject);
 
       connect(
-        button_open, &QPushButton::clicked, this, &ProjectWindow::OpenProject);
+        button_open, 
+        &QPushButton::clicked, 
+        this, 
+        &ProjectWindow::OnOpenProject);
 
       connect(button_cancel, &QPushButton::clicked, this, &QDialog::reject);
 
       QSettings& settings = EditorApplication::Instance()->GlobalSettings();
       recent_folder_ = settings.value(kSettingsRecentFolder_, "").toString();
+
+      LoadRecentlyOpened();
     }
 
     //--------------------------------------------------------------------------
-    void ProjectWindow::NewProject()
-    {
-      
-    }
-
-    //--------------------------------------------------------------------------
-    void ProjectWindow::OpenProject()
+    void ProjectWindow::OnNewProject()
     {
       bool valid;
       QString dir = ShowFolderDialog(&valid);
+
+      if (valid == false)
+      {
+        return;
+      }
+
+      QString settings_path = dir + "/" + kProjectFile_;
+
+      if (QFile::exists(settings_path) == true)
+      {
+        QMessageBox error;
+        error.setText("Cannot create project");
+        error.setInformativeText(
+          "There is already a project in this directory");
+        error.setStandardButtons(QMessageBox::Ok);
+        error.setDefaultButton(QMessageBox::Ok);
+        error.setIcon(QMessageBox::Icon::Critical);
+
+        QSpacerItem* spacer = new QSpacerItem(
+          kProjectWindowWidth_  * 0.5, 
+          0, 
+          QSizePolicy::Minimum, 
+          QSizePolicy::Expanding);
+
+        QGridLayout* layout = static_cast<QGridLayout*>(error.layout());
+        layout->addItem(
+          spacer, 
+          layout->rowCount(), 
+          0,
+          1,
+          layout->columnCount());
+
+        // Hack to dodge a Qt error message..?
+        error.setFixedSize(640, 480);
+        error.exec();
+
+        return;
+      }
+
+      std::unique_ptr<NewProjectDialog> dialog(new NewProjectDialog(this));
+
+      if (dialog->exec() == QDialog::Rejected)
+      {
+        return;
+      }
+
+      QJsonDocument doc;
+      QJsonObject root;
+      root.insert("project_name", dialog->project_name());
+
+      doc.setObject(root);
+
+      QFile file(settings_path);
+
+      if (file.open(QFile::WriteOnly) == true)
+      {
+        file.write(doc.toJson());
+        AddRecentProject(dir);
+      }
+    }
+
+    //--------------------------------------------------------------------------
+    void ProjectWindow::OnOpenProject()
+    {
+      bool valid;
+      QString dir = ShowFolderDialog(&valid);
+    }
+
+    //--------------------------------------------------------------------------
+    void ProjectWindow::LoadRecentlyOpened()
+    {
+      QSettings& settings = EditorApplication::Instance()->GlobalSettings();
+      
+      bool found = true;
+
+      QString formatted;
+      QString path;
+      int count = 0;
+
+      while (
+        found == true && 
+        count != kMaxRecentlyOpened_)
+      {
+        found = false;
+        formatted = kSettingsRecentlyOpened_.arg(count);
+
+        if (settings.contains(formatted) == true)
+        {
+          found = true;
+          path = settings.value(formatted).toString();
+          recent_project_paths_.push_back(path);
+        }
+
+        ++count;
+      }
+
+      RefreshRecentlyOpened();
+    }
+
+    //--------------------------------------------------------------------------
+    void ProjectWindow::RefreshRecentlyOpened()
+    {
+      recent_projects_->clear();
+
+      QString path;
+      for (int i = recent_project_paths_.size() - 1; i >= 0; --i)
+      {
+        path = recent_project_paths_.at(i) + "/" + kProjectFile_;
+
+        if (QFile::exists(path) == false)
+        {
+          recent_project_paths_.erase(recent_project_paths_.begin() + i);
+        }
+      }
+
+      for (int i = 0; i < recent_project_paths_.size(); ++i)
+      {
+        if (i >= kMaxRecentlyOpened_)
+        {
+          break;
+        }
+
+        recent_projects_->addItem(recent_project_paths_.at(i));
+      }
+    }
+
+    //--------------------------------------------------------------------------
+    void ProjectWindow::AddRecentProject(const QString& path)
+    {
+      for (int i = 0; i < recent_project_paths_.size(); ++i)
+      {
+        if (recent_project_paths_.at(i) == path)
+        {
+          recent_project_paths_.erase(recent_project_paths_.begin() + i);
+          break;
+        }
+      }
+
+      recent_project_paths_.push_front(path);
+
+      RefreshRecentlyOpened();
     }
     
     //--------------------------------------------------------------------------
@@ -122,6 +284,116 @@ namespace snuffbox
       }
 
       return dir;
+    }
+
+    //--------------------------------------------------------------------------
+    void ProjectWindow::SaveRecentProjects()
+    {
+      QSettings& settings = EditorApplication::Instance()->GlobalSettings();
+
+      int save_at = 0;
+      for (int i = 0; i < kMaxRecentlyOpened_; ++i)
+      {
+        if (i < recent_project_paths_.size())
+        {
+          const QString& path = recent_project_paths_.at(i);
+          if (QFile::exists(path + "/" + kProjectFile_) == true)
+          {
+            settings.setValue(
+              kSettingsRecentlyOpened_.arg(save_at),
+              path);
+
+            ++save_at;
+          }
+        }
+      }
+      
+      for (int i = kMaxRecentlyOpened_ - 1; i > save_at; --i)
+      {
+        settings.remove(kSettingsRecentlyOpened_.arg(i));
+      }
+    }
+
+    //--------------------------------------------------------------------------
+    ProjectWindow::~ProjectWindow()
+    {
+      SaveRecentProjects();
+    }
+
+    //--------------------------------------------------------------------------
+    NewProjectDialog::NewProjectDialog(QWidget* parent) :
+      QDialog(parent),
+      project_name_("new-project"),
+      button_create_(nullptr)
+    {
+      int w = kNewProjectDialogWidth_;
+      int h = kNewProjectDialogHeight_;
+
+      setMinimumSize(w, h);
+      setMaximumSize(w, h);
+
+      QVBoxLayout* main_layout = new QVBoxLayout(this);
+      main_layout->setObjectName(QStringLiteral("NewProjectMainLayout"));
+
+      error_ = new QLabel(main_layout->widget());
+      error_->setText(QStringLiteral("Please specify a name"));
+      error_->setStyleSheet("QLabel { color: red; }");
+      error_->setVisible(false);
+
+      main_layout->addWidget(error_);
+
+      QLineEdit* input_box = new QLineEdit(main_layout->widget());
+      input_box->setObjectName(QStringLiteral("NewProjectInputBox"));
+
+      input_box->setText(project_name_);
+
+      main_layout->addWidget(input_box);
+
+      QHBoxLayout* button_layout = new QHBoxLayout(main_layout->widget());
+      button_layout->setObjectName(QStringLiteral("NewProjectButtonLayout"));
+
+      button_create_ = 
+        new QPushButton(QStringLiteral("Create"), button_layout->widget());
+      button_create_->setObjectName(QStringLiteral("NewProjectButtonCreate"));
+
+      QPushButton* button_cancel = 
+        new QPushButton(QStringLiteral("Cancel"), button_layout->widget());
+      button_cancel->setObjectName(QStringLiteral("NewProjectButtonCancel"));
+
+      button_layout->addWidget(button_create_);
+      button_layout->addWidget(button_cancel);
+
+      main_layout->addLayout(button_layout);
+
+      connect(button_create_, &QPushButton::clicked, this, &QDialog::accept);
+      connect(button_cancel, &QPushButton::clicked, this, &QDialog::reject);
+
+      connect(
+        input_box, 
+        &QLineEdit::textChanged, 
+        this, 
+        &NewProjectDialog::OnNameChanged);
+    }
+
+    //--------------------------------------------------------------------------
+    const QString& NewProjectDialog::project_name() const
+    {
+      return project_name_;
+    }
+
+    //--------------------------------------------------------------------------
+    void NewProjectDialog::OnNameChanged(const QString& name)
+    {
+      project_name_ = name;
+      bool valid = true;
+
+      if (name.isEmpty() == true || name.startsWith(' ') == true)
+      {
+        valid = false;
+      }
+
+      button_create_->setEnabled(valid == true);
+      error_->setVisible(valid == false);
     }
   }
 }
--- src/tools/editor/project/project_window.h
+++ src/tools/editor/project/project_window.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <QDialog>
+#include <QList>
+
+class QLabel;
+class QListWidget;
 
 namespace snuffbox
 {
@@ -35,12 +39,31 @@ namespace snuffbox
       /**
       * @brief Create a 'new project' dialog
       */
-      void NewProject();
+      void OnNewProject();
 
       /**
       * @brief Create an 'open project' dialog
       */
-      void OpenProject();
+      void OnOpenProject();
+
+    protected:
+
+      /**
+      * @brief Load recently opened projects and show them in the window
+      */
+      void LoadRecentlyOpened();
+
+      /**
+      * @brief Refreshes the recently opened list widget
+      */
+      void RefreshRecentlyOpened();
+
+      /**
+      * @brief Adds a project to the list of recently opened proejcts
+      *
+      * @param[in] path The path to the recent project to add
+      */
+      void AddRecentProject(const QString& path);
 
       /**
       * @brief General folder dialog that shows a file picker that only accepts
@@ -54,10 +77,28 @@ namespace snuffbox
       */
       QString ShowFolderDialog(bool* valid);
 
+      /**
+      * @brief Saves the recently opened projects
+      */
+      void SaveRecentProjects();
+
+    public:
+
+      /**
+      * @brief Save all on-close settings in the destructor
+      */
+      ~ProjectWindow();
+
     private:
 
       Project* project_; //!< A reference to the current project settings
       QString recent_folder_; //!< The last folder that was opened in a dialog
+      QListWidget* recent_projects_; //!< The list of recent projects
+
+      /**
+      * @brief The list of all loaded recent projects
+      */
+      QList<QString> recent_project_paths_;
 
       static const int kProjectWindowWidth_; //!< The width of the dialog
       static const int kProjectWindowHeight_; //!< The height of the dialog
@@ -66,6 +107,70 @@ namespace snuffbox
       * @brief The key for the recent folder setting, in the global QSettings
       */
       static const QString kSettingsRecentFolder_;
+
+      /**
+      * @brief The key for the recently opened projects, in the global QSettings
+      */
+      static const QString kSettingsRecentlyOpened_;
+
+      /**
+      * @brief The file to save project settings in
+      */
+      static const QString kProjectFile_;
+
+      /**
+      * @brief The maximum number of recently opened projects to show
+      */
+      static const int kMaxRecentlyOpened_;
+    };
+
+    /**
+    * @brief This dialog is shown when the user creates a new project
+    *
+    * When no name is specified for the project, the user is not allowed
+    * to continue
+    *
+    * @author Daniel Konings
+    */
+    class NewProjectDialog : public QDialog
+    {
+
+      Q_OBJECT;
+
+    public:
+
+      /**
+      * @brief Construct through a parent widget
+      *
+      * @param[in] parent The parent widget that owns this dialog
+      */
+      NewProjectDialog(QWidget* parent = nullptr);
+
+      /**
+      * @return The current project name
+      */
+      const QString& project_name() const;
+
+    protected slots:
+
+      /**
+      * @brief Called when the input box's text was changed. This validates
+      *        the text as a valid project name, it also sets the
+      *        NewProjectDialog::project_name_ member
+      *
+      * @param[in] name The name of the project
+      */
+      void OnNameChanged(const QString& name);
+
+    private:
+
+      QString project_name_; //!< The current project name
+
+      QPushButton* button_create_; //!< The create button
+      QLabel* error_; //!< The current error message
+
+      static const int kNewProjectDialogWidth_; //!< The width of the dialog
+      static const int kNewProjectDialogHeight_; //!< The height of the dialog
     };
   }
 }
