27b55aa2cfc1fb9aaf086905ed92c49c09232e2d
Daniel Konings <konings_304@hotmail.com>
2019-11-03 13:57:22 +0100

    - Should be basically done with the PropertyValue implementation

---
+++ src/tools/editor/property-editor/property_value.cc
@@ -0,0 +1,214 @@
+#include "tools/editor/property-editor/property_value.h"
+
+namespace snuffbox
+{
+  namespace editor
+  {
+    //--------------------------------------------------------------------------
+    bool PropertyValue::IsNumber() const
+    {
+      return false;
+    }
+
+    //--------------------------------------------------------------------------
+    bool PropertyValue::IsString() const
+    {
+      return false;
+    }
+
+    //--------------------------------------------------------------------------
+    bool PropertyValue::IsUUID() const
+    {
+      return false;
+    }
+
+    //--------------------------------------------------------------------------
+    bool PropertyValue::IsVector(int length) const
+    {
+      return false;
+    }
+
+    //--------------------------------------------------------------------------
+    bool PropertyValue::IsAsset() const
+    {
+      return false;
+    }
+
+    //--------------------------------------------------------------------------
+    bool PropertyValue::IsList() const
+    {
+      return false;
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyValue::Set(void* object, const double& value)
+    {
+      foundation::Logger::Assert(IsNumber() == true, 
+        "Attempted to set a number value on a non-number property");
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyValue::Set(void* object, const foundation::String& value)
+    {
+      foundation::Logger::Assert(IsString() == true,
+        "Attempted to set a string value on a non-string property");
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyValue::Set(void* object, const foundation::UUID& value)
+    {
+      foundation::Logger::Assert(IsUUID() == true,
+        "Attempted to set a UUID value on a non-UUID property");
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyValue::Set(void* object, const glm::vec2& value)
+    {
+      foundation::Logger::Assert(IsVector(2) == true,
+        "Attempted to set a vec2 value on a non-vec2 property");
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyValue::Set(void* object, const glm::vec3& value)
+    {
+      foundation::Logger::Assert(IsVector(3) == true,
+        "Attempted to set a vec3 value on a non-vec3 property");
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyValue::Set(void* object, const glm::vec4& value)
+    {
+      foundation::Logger::Assert(IsVector(4) == true,
+        "Attempted to set a vec4 value on a non-vec4 property");
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyValue::Set(
+      void* object, 
+      const engine::SerializableAsset& value)
+    {
+      foundation::Logger::Assert(IsAsset() == true,
+        "Attempted to set an asset value on a non-asset property");
+    }
+
+    //--------------------------------------------------------------------------
+    PropertyList::PropertyList() :
+      was_changed_(false)
+    {
+
+    }
+
+    //--------------------------------------------------------------------------
+    bool PropertyList::IsList() const
+    {
+      return true;
+    }
+
+    //--------------------------------------------------------------------------
+    bool PropertyList::HasChanged(void* object)
+    {
+      bool changed = false;
+      if (was_changed_ == false)
+      {
+        for (size_t i = 0; i < values_.size(); ++i)
+        {
+          if (values_.at(i)->HasChanged(object) == true)
+          {
+            changed = true;
+            break;
+          }
+        }
+      }
+      else
+      {
+        was_changed_ = false;
+        changed = true;
+      }
+
+      return changed;
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyList::Add(const foundation::SharedPtr<PropertyValue>& value)
+    {
+      values_.push_back(value);
+      was_changed_ = true;
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyList::Insert(
+      int index, 
+      const foundation::SharedPtr<PropertyValue>& value)
+    {
+      values_.insert(values_.begin() + index, value);
+      was_changed_ = true;
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyList::Remove(const foundation::SharedPtr<PropertyValue>& value)
+    {
+      int idx = IndexOf(value);
+
+      if (idx < 0)
+      {
+        return;
+      }
+
+      values_.erase(values_.begin() + idx);
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyList::RemoveAt(int index)
+    {
+      if (index < 0 || index >= Count())
+      {
+        return;
+      }
+
+      values_.erase(values_.begin() + index);
+    }
+
+    //--------------------------------------------------------------------------
+    int PropertyList::IndexOf(const foundation::SharedPtr<PropertyValue>& value)
+    {
+      for (int i = 0; i < values_.size(); ++i)
+      {
+        if (values_.at(i) == value)
+        {
+          return i;
+        }
+      }
+
+      return -1;
+    }
+
+    //--------------------------------------------------------------------------
+    foundation::SharedPtr<PropertyValue> PropertyList::At(int index) const
+    {
+      if (index < 0 || index >= Count())
+      {
+        return nullptr;
+      }
+
+      return values_.at(index);
+    }
+
+    //--------------------------------------------------------------------------
+    int PropertyList::Count() const
+    {
+      return static_cast<int>(values_.size());
+    }
+
+    //--------------------------------------------------------------------------
+    void PropertyList::Clear()
+    {
+      values_.clear();
+    }
+
+    //--------------------------------------------------------------------------
+    PropertyList::~PropertyList()
+    {
+      Clear();
+    }
+  }
+}
--- src/tools/editor/property-editor/property_value.h
+++ src/tools/editor/property-editor/property_value.h
@@ -31,75 +31,313 @@ namespace snuffbox
 
     public:
 
+      /**
+      * @return Is this property a number type?
+      */
       virtual bool IsNumber() const;
+
+      /**
+      * @return Is this property a string type?
+      */
       virtual bool IsString() const;
+
+      /**
+      * @return Is this property a UUID type?
+      */
       virtual bool IsUUID() const;
+
+      /**
+      * @brief Checks if this property is a specific vector type
+      *
+      * @param[in] length The length to check for
+      *
+      * @return Is this property a Vec2, Vec3 or Vec4 type?
+      */
       virtual bool IsVector(int length) const;
+
+      /**
+      * @return Is this property an asset type?
+      */
       virtual bool IsAsset() const;
+
+      /**
+      * @return Is this property a list of different properties?
+      */
       virtual bool IsList() const;
-      virtual bool IsUserData() const;
+
+      /**
+      * @brief Checks if this specific property within an object has been
+      *        changed
+      *
+      * @param[in] object The object to operate on
+      *
+      * @return Has this property for that object been changed?
+      *
+      * @remarks This should automatically update the value within the
+      *          property, so that a newer call will evaluate to false
+      */
+      virtual bool HasChanged(void* object) = 0;
+
+      /**
+      * @brief Sets a number value
+      *
+      * @param[in] object The object to operate on
+      * @param[in] value The new value
+      */
+      virtual void Set(void* object, const double& value);
+
+      /**
+      * @brief Sets a string value
+      *
+      * @param[in] object The object to operate on
+      * @param[in] value The new value
+      */
+      virtual void Set(void* object, const foundation::String& value);
+
+      /**
+      * @brief Sets a UUID value
+      *
+      * @param[in] object The object to operate on
+      * @param[in] value The new value
+      */
+      virtual void Set(void* object, const foundation::UUID& value);
+
+      /**
+      * @brief Sets a Vec2 value
+      *
+      * @param[in] object The object to operate on
+      * @param[in] value The new value
+      */
+      virtual void Set(void* object, const glm::vec2& value);
+
+      /**
+      * @brief Sets a Vec3 value
+      *
+      * @param[in] object The object to operate on
+      * @param[in] value The new value
+      */
+      virtual void Set(void* object, const glm::vec3& value);
+
+      /**
+      * @brief Sets a Vec4 value
+      *
+      * @param[in] object The object to operate on
+      * @param[in] value The new value
+      */
+      virtual void Set(void* object, const glm::vec4& value);
+
+      /**
+      * @brief Sets an asset value
+      *
+      * @param[in] object The object to operate on
+      * @param[in] value The new value
+      */
+      virtual void Set(void* object, const engine::SerializableAsset& value);
     };
 
+    /**
+    * @brief A property strongly typed property value, with both the
+    *        type of the property and the type of the object to operate on
+    *
+    * @tparam T The type of the property
+    * @tparam Y The type of the object to operate on
+    *
+    * @author Daniel Konings
+    */
     template <typename T, typename Y>
     class Property : public PropertyValue
     {
 
     public:
 
+      /**
+      * @brief A type definition for the setter data binding
+      */
       typedef void(*Setter)(Y*, const T&);
+
+      /**
+      * @brief A type definition for the getter data binding
+      */
       typedef T(*Getter)(Y*);
 
+      /**
+      * @brief Delete default constructor
+      */
       Property() = delete;
-      Property(const T& default_value);
 
+      /**
+      * @brief Construct through a setter binding and a getter binding
+      *
+      * @param[in] setter The setter binding
+      * @param[in] getter The getter binding
+      */
+      Property(Setter setter, Getter getter);
+
+      /**
+      * @see PropertyValue::IsNumber
+      */
       bool IsNumber() const override;
+
+      /**
+      * @see PropertyValue::IsString
+      */
       bool IsString() const override;
+
+      /**
+      * @see PropertyValue::IsUUID
+      */
       bool IsUUID() const override;
+
+      /**
+      * @see PropertyValue::IsVector
+      */
       bool IsVector(int length) const override;
+
+      /**
+      * @see PropertyValue::IsAsset
+      */
       bool IsAsset() const override;
-      bool IsList() const override;
-      bool IsUserData() const override;
+
+      /**
+      * @see PropertyValue::HasChanged
+      */
+      bool HasChanged(void* object) override;
+
+      /**
+      * @see PropertyValue::Set
+      */
+      void Set(void* object, const T& value) override;
+
+      /**
+      * @brief Retrieves a value from within the object that is being operated
+      *        on, if a getter is defined
+      *
+      * @remarks This returns a default constructed value if no getter is set
+      *
+      * @param[in] object The object to operate on
+      *
+      * @return The retrieved value
+      */
+      T Get(Y* object) const;
+
+    private:
+
+      T value_; //!< The value contained in this property to check for changes
+      Setter setter_; //!< The setter binding
+      Getter getter_; //!< The getter binding
     };
 
+    /**
+    * @brief Defines a list of properties, which in turn can be used as
+    *        a PropertyValue again
+    *
+    * @remarks All contained values are ref counted using shared pointers
+    *
+    * @author Daniel Konings
+    */
     class PropertyList : public PropertyValue
     {
 
     public:
 
-      PropertyList() = delete;
-      PropertyList(const foundation::String& name);
+      /**
+      * @brief Default constructor
+      */
+      PropertyList();
 
+      /**
+      * @see PropertyValue::IsList
+      */
       bool IsList() const override;
 
-      void Add(PropertyValue* value);
-      void Insert(int index, PropertyValue* value);
-      void Remove(PropertyValue* value);
+      /**
+      * @see PropertyValue::HasChanged
+      */
+      bool HasChanged(void* object) override;
+
+      /**
+      * @brief Adds a new property value to the list
+      *
+      * @param[in] value The value to add
+      */
+      void Add(const foundation::SharedPtr<PropertyValue>& value);
+
+      /**
+      * @brief Inserts a new property at a provided index
+      *
+      * @param[in] index The index to insert at
+      * @param[in] value The value to insert
+      */
+      void Insert(
+        int index, 
+        const foundation::SharedPtr<PropertyValue>& value);
+
+      /**
+      * @brief Removes a property value by value
+      *
+      * @param[in] value The value to remove
+      */
+      void Remove(const foundation::SharedPtr<PropertyValue>& value);
+
+      /**
+      * @brief Removes a property value at a given index
+      *
+      * @param[in] index The index of the property value to remove
+      */
       void RemoveAt(int index);
 
-      size_t IndexOf(PropertyValue* value);
-      foundation::SharedPtr<PropertyValue> At(int index)const;
-      size_t Size() const;
+      /**
+      * @brief Finds the index of a provided property value
+      *
+      * @param[in] value The value to find
+      *
+      * @return The index of the item, or -1 if it was not found
+      */
+      int IndexOf(const foundation::SharedPtr<PropertyValue>& value);
 
+      /**
+      * @brief Retrieves a property value at the specified index
+      *
+      * @param[in] index The index of the property value to retrieve
+      *
+      * @return The retrieved property value, or nullptr if the index
+      *         was out of bounds
+      */
+      foundation::SharedPtr<PropertyValue> At(int index) const;
+
+      /**
+      * @return The number of property values stored within this list
+      */
+      int Count() const;
+
+      /**
+      * @brief Clears all property values in this list, reducing their ref
+      *        count by 1
+      */
       void Clear();
+
+      /**
+      * @brief Clears the entire list
+      */
+      ~PropertyList();
 
     private:
 
+      bool was_changed_; //!< Was this list changed?
+
+      /**
+      * @brief The entire list of property values
+      */
       foundation::Vector<foundation::SharedPtr<PropertyValue>> values_;
     };
 
     //--------------------------------------------------------------------------
     template <typename T, typename Y>
-    inline bool Property<T, Y>::IsUserData() const
+    inline Property<T, Y>::Property(Setter setter, Getter getter) :
+      setter_(setter),
+      getter_(getter)
     {
-      return 
-        IsNumber() == false && 
-        IsString() == false && 
-        IsUUID() == false && 
-        IsVector(2) == false &&
-        IsVector(3) == false &&
-        IsVector(4) == false &&
-        IsAsset() == false &&
-        IsList() == false;
+
     }
 
     //--------------------------------------------------------------------------
@@ -148,6 +386,50 @@ namespace snuffbox
     inline bool Property<T, Y>::IsAsset() const
     {
       return eastl::is_same<engine::SerializableAsset, T>::value;
+    }
+
+    //--------------------------------------------------------------------------
+    template <typename T, typename Y>
+    inline bool Property<T, Y>::HasChanged(void* object)
+    {
+      Y* ptr = reinterpret_cast<Y*>(object);
+      T new_value = Get(ptr);
+
+      bool changed = value_ == new_value;
+
+      if (changed == true)
+      {
+        value_ = new_value;
+      }
+
+      return changed;
+    }
+
+    //--------------------------------------------------------------------------
+    template <typename T, typename Y>
+    inline void Property<T, Y>::Set(void* object, const T& value)
+    {
+      if (setter_ == nullptr)
+      {
+        return;
+      }
+
+      Y* ptr = reinterpret_cast<Y*>(object);
+      setter_(ptr, value);
+
+      value_ = value;
+    }
+
+    //--------------------------------------------------------------------------
+    template <typename T, typename Y>
+    inline T Property<T, Y>::Get(Y* object) const
+    {
+      if (getter_ == nullptr)
+      {
+        return T();
+      }
+
+      return getter_(object);
     }
   }
 }
